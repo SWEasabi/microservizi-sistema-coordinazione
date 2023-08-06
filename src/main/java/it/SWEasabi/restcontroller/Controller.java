047d911787d43a45c7ec517a3a897535f47bf691
casYy <mattia.casarotto.16@gmail.com>
2023-08-06 20:45:58 +0200

    endpoint

--- src/main/java/it/SWEasabi/restcontroller/Controller.java
+++ src/main/java/it/SWEasabi/restcontroller/Controller.java
@@ -2,6 +2,7 @@ package it.SWEasabi.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,16 @@ public class Controller {
 	@Autowired
 	MqttController mqttController;
 	
+	@CrossOrigin (origins = "http://localhost:4200", maxAge = 3600)
 	@PostMapping("/setIlluminazione/{idLamp}/{value}")
-	public void SetIlluminazione(@PathVariable long idLamp, @PathVariable int value)
+	public boolean SetIlluminazione(@PathVariable long idLamp, @PathVariable int value)
 	{
-		mqttController.apiArrived(idLamp, value);
+		try {
+			mqttController.apiArrived(idLamp, value);
+			return true;
+		}
+		catch(Exception e) {
+			return false;
+		}
 	}
 }
