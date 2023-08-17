1e48f13fe292ee707bcf4ff43bd709fb33a1033c
Luca Pierobon <pierobonluca01@gmail.com>
2023-08-17 15:05:28 +0200

    Aggiunti test anagrafica

---
+++ src/test/java/it/SWEasabi/modelli/anagrafica/AreaAnagraficaTest.java
@@ -0,0 +1,48 @@
+package it.SWEasabi.modelli.anagrafica;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AreaAnagraficaTest {
+
+    AreaAnagrafica area;
+    AreaAnagrafica area_auto;
+
+    @Before
+    public void setUp() throws Exception {
+        area = new AreaAnagrafica();
+        area_auto = new AreaAnagrafica(1, "area_auto", true, 0, 100);
+    }
+
+    @Test
+    public void testGetId() {
+        assertEquals(0, area.getId());
+        assertEquals(1, area_auto.getId());
+    }
+
+    @Test
+    public void testGetNome() {
+        assertEquals("", area.getNome());
+        assertEquals("area_auto", area_auto.getNome());
+    }
+
+    @Test
+    public void testGetModAutomatica() {
+        assertFalse(area.getModAutomatica());
+        assertTrue(area_auto.getModAutomatica());
+    }
+
+    @Test
+    public void testGetlvlinf() {
+        assertEquals(0, area.getLvlInf());
+        assertEquals(0, area_auto.getLvlInf());
+    }
+
+    @Test
+    public void testGetlvlsup() {
+        assertEquals(0, area.getLvlSup());
+        assertEquals(100, area_auto.getLvlSup());
+    }
+}
---
+++ src/test/java/it/SWEasabi/modelli/anagrafica/LampAnagraficaTest.java
@@ -0,0 +1,104 @@
+package it.SWEasabi.modelli.anagrafica;
+
+import it.SWEasabi.modelli.anagrafica.LampAnagrafica;
+import it.SWEasabi.modelli.anagrafica.Misuratore;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LampAnagraficaTest {
+
+    Misuratore misuratore;
+    LampAnagrafica lampione;
+    LampAnagrafica lampione100;
+
+    @Before
+    public void setUp() throws Exception {
+        misuratore = new Misuratore();
+
+        lampione = new LampAnagrafica();
+        lampione100 = new LampAnagrafica(100, 100);
+        lampione.setMisuratore(misuratore);
+    }
+
+    @Test
+    public void testGetMisuratore() {
+        assertEquals(misuratore, lampione.getMisuratore());
+    }
+
+    @Test
+    public void testSetMisuratore() {
+        Misuratore misuratore1 = new Misuratore();
+        misuratore1.setId(1);
+
+        lampione.setMisuratore(misuratore1);
+        assertEquals(misuratore1, lampione.getMisuratore());
+        lampione.setMisuratore(misuratore);
+        assertEquals(misuratore, lampione.getMisuratore());
+    }
+
+    @Test
+    public void testGetId() {
+        assertEquals(0, lampione.getId());
+    }
+
+    @Test
+    public void testSetId() {
+        lampione.setId(1);
+        assertEquals(1, lampione.getId());
+        lampione.setId(0);
+        assertEquals(0, lampione.getId());
+    }
+
+    @Test
+    public void testGetWattaggio() {
+        assertEquals(100, lampione100.getWattaggio());
+    }
+
+    @Test
+    public void testSetWattaggio() {
+        lampione100.setWattaggio(0);
+        assertEquals(0, lampione100.getWattaggio());
+        lampione100.setWattaggio(100);
+        assertEquals(100, lampione100.getWattaggio());
+    }
+
+    @Test
+    public void testGetLuminosita() {
+        assertEquals(100, lampione100.getLuminosita());
+    }
+
+    @Test
+    public void testSetLuminosita() {
+        lampione100.setLuminosita(100);
+        assertEquals(100, lampione100.getLuminosita());
+        lampione100.setLuminosita(0);
+        assertEquals(0, lampione100.getLuminosita());
+    }
+
+    @Test
+    public void testNullObj() {
+        Object obj = null;
+        assertFalse(lampione.equals(obj));
+    }
+
+    @Test
+    public void testObjClass() {
+        Object obj = new Misuratore();
+        assertFalse(lampione.equals(obj));
+    }
+
+    @Test
+    public void testNotEquals() {
+        assertFalse(lampione.equals(lampione100));
+    }
+
+    @Test
+    public void testEquals() {
+        LampAnagrafica lampione0 = new LampAnagrafica();
+        lampione0.setMisuratore(misuratore);
+
+        assertTrue(lampione.equals(lampione0));
+    }
+}
